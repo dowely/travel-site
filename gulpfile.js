70d8efbed3f44d670f810f956817a7925bd12cc2
Marcin <marcin.ogar@gmail.com>
2017-11-11 14:48:05 +0100

    Organized css and gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,2 @@
-var gulp = require('gulp'),
-    watch = require('gulp-watch');
-
-gulp.task('default', function(){
-  console.log('Hooray! - You rule ;)');
-});
-
-gulp.task('html', function(){
-  console.log('imagine something useful being done to your html file');
-});
-
-gulp.task('css', function(){
-  console.log('imagine something useful being done to your css file');
-});
-
-gulp.task('watch', function(){
-
-  watch('./app/index.html', function(){
-    gulp.start('html');
-  });
-
-  watch('./app/assets/styles/**/*.css', function(){
-    gulp.start('css');
-  });
-
-});
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
